f2e6c1be2cebd0f6d6bbba62627b039834838ec2
LeVanChung19319 <86992800+LeVanChung19319@users.noreply.github.com>
2022-03-16 23:38:15 +0700

    restore test

--- app/src/main/java/com/example/eaterydemo/HomeActivity.java
+++ app/src/main/java/com/example/eaterydemo/HomeActivity.java
@@ -3,12 +3,14 @@ package com.example.eaterydemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_home);
+
     }
 }
