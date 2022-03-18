11e3886cadb525012890f48224f0edc904ff1e31
Khoattdps19321 <khoattdps19321@fpt.edu.vn>
2022-03-18 16:05:22 +0700

    Giao diện deadline
    Màn hình yêu thích Light, chi tiết nhà hàng, chi tiết món ăn, thanh toán thành công Light, item món ăn, item nhà hàng

--- app/src/main/java/com/example/eaterydemo/MainActivity.java
+++ app/src/main/java/com/example/eaterydemo/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.eaterydemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    CardView card, monan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.monan_item);
     }
 }
